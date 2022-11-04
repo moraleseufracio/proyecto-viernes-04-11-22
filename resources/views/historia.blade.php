1218c41ab637d84d311cffbd5fbbd897128be498
moraleseufracio <1367162@senati.pe>
2022-11-04 16:13:38 -0500

    semi final

---
+++ resources/views/historia.blade.php
@@ -0,0 +1,21 @@
+<?php
+echo "SOY LA HISTORIA"
+?>
+
+<body>
+<select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+  <option selected>inicio</option>
+  <option value="1">uno</option>
+  <option value="2">dos</option>
+  <option value="3">tres</option>
+</select>
+
+<select class="form-select form-select-sm" aria-label=".form-select-sm example">
+  <option selected>menu</option>
+  <option value="1">uno</option>
+  <option value="2">dos</option>
+  <option value="3">Tres</option>
+</select>  
+
+
+</body>
